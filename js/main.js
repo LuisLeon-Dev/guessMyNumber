1e8db2951aad35de9ce7d1ec5091ec3020627038
LuisLeon-Dev <lleon5073@gmail.com>
2023-12-04 11:42:16 -0600

    refactoring code

--- js/main.js
+++ js/main.js
@@ -11,34 +11,30 @@ let pointsLeft = 20;
 let highScore = 0;
 
 let numberToGuess = Math.floor(Math.random() * 20) + 1;
-console.log(numberToGuess);
+
+function display(message, background, width, number) {
+  message.textContent = message;
+  backgroundColor.style.backgroundColor = background;
+  secretNumber.style.width = width;
+  secretNumber.textContent = number;
+}
 
 checkBtn.addEventListener("click", function () {
   const guess = Number(inputGuess.value);
   if (!guess) {
     message.textContent = "Insert a number...";
   } else if (guess === numberToGuess) {
-    message.textContent = "Correct number. You won!";
-    backgroundColor.style.backgroundColor = "#60b347";
-    secretNumber.style.width = "30rem";
-    secretNumber.textContent = numberToGuess;
+    display("Correct number. You won!", "#60b347", "30rem", numberToGuess);
 
     if (pointsLeft > highScore) {
       highScore = pointsLeft;
       highScoreL.textContent = highScore;
     }
-  } else if (guess > numberToGuess) {
+  } else if (guess !== numberToGuess) {
     if (pointsLeft > 1) {
-      message.textContent = "Too high!";
-      pointsLeft--;
-      score.textContent = pointsLeft;
-    } else {
-      message.textContent = "You lost!";
-      score.textContent = 0;
-    }
-  } else if (guess < numberToGuess) {
-    if (pointsLeft > 1) {
-      message.textContent = "Too low!";
+      guess > numberToGuess
+        ? (message.textContent = "Too high!")
+        : (message.textContent = "Too low!");
       pointsLeft--;
       score.textContent = pointsLeft;
     } else {
@@ -53,11 +49,7 @@ restartBtn.addEventListener("click", function () {
   pointsLeft = 20;
   score.textContent = pointsLeft;
   numberToGuess = Math.floor(Math.random() * 20) + 1;
-  console.log(numberToGuess);
 
   inputGuess.value = "";
-  message.textContent = "Start guessing...";
-  backgroundColor.style.backgroundColor = "#222";
-  secretNumber.textContent = "?";
-  secretNumber.style.width = "15rem";
+  display("Start guessing...", "#222", "15rem", "?");
 });
