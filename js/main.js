2465ce7d669dfb0c0067e828020591685c566570
LuisLeon-Dev <lleon5073@gmail.com>
2023-12-04 11:31:49 -0600

    implementing high scores

--- js/main.js
+++ js/main.js
@@ -4,24 +4,14 @@ const inputGuess = document.querySelector(".guess");
 const message = document.querySelector(".message");
 const secretNumber = document.querySelector(".number");
 const score = document.querySelector(".score");
+const highScoreL = document.querySelector(".highscore");
 const backgroundColor = document.querySelector("body");
 
 let pointsLeft = 20;
+let highScore = 0;
 
 let numberToGuess = Math.floor(Math.random() * 20) + 1;
 console.log(numberToGuess);
-
-restartBtn.addEventListener("click", function () {
-  //window.location.reload();
-  score.textContent = 20;
-  numberToGuess = Math.floor(Math.random() * 20) + 1;
-
-  inputGuess.value = "";
-  message.textContent = "Start guessing...";
-  backgroundColor.style.backgroundColor = "#222";
-  secretNumber.textContent = "?";
-  secretNumber.style.width = "15rem";
-});
 
 checkBtn.addEventListener("click", function () {
   const guess = Number(inputGuess.value);
@@ -32,6 +22,11 @@ checkBtn.addEventListener("click", function () {
     backgroundColor.style.backgroundColor = "#60b347";
     secretNumber.style.width = "30rem";
     secretNumber.textContent = numberToGuess;
+
+    if (pointsLeft > highScore) {
+      highScore = pointsLeft;
+      highScoreL.textContent = highScore;
+    }
   } else if (guess > numberToGuess) {
     if (pointsLeft > 1) {
       message.textContent = "Too high!";
@@ -51,4 +46,18 @@ checkBtn.addEventListener("click", function () {
       score.textContent = 0;
     }
   }
+});
+
+restartBtn.addEventListener("click", function () {
+  //window.location.reload();
+  pointsLeft = 20;
+  score.textContent = pointsLeft;
+  numberToGuess = Math.floor(Math.random() * 20) + 1;
+  console.log(numberToGuess);
+
+  inputGuess.value = "";
+  message.textContent = "Start guessing...";
+  backgroundColor.style.backgroundColor = "#222";
+  secretNumber.textContent = "?";
+  secretNumber.style.width = "15rem";
 });
